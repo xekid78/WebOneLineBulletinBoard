7076cbdbb57ae354ded0519f1f29fcad7227d2c7
Makoto Tanabe <xekid78@outlook.com>
2018-05-07 22:07:56 +0900

    INSERT statement added

--- php/pdo.php
+++ php/pdo.php
@@ -1,13 +1,33 @@
 <?php
+// DB接続
 define('DB_DATABASE', 'lesson');
 define('DB_USERNAME', 'user01');
 define('DB_PASSWORD', 'vagrant');
 define('PDO_DSN', 'mysql:dbhost=localhost;dbname='.DB_DATABASE);
 
 try {
-    $db = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWORD);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWORD);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo $e->getMessage();
     exit;
 }
+
+// 入力データ
+$content = $_POST["content"];
+
+// DB挿入
+try {
+    $sql  = "INSERT INTO bbs (content, updated_at) VALUES (?, ?);";
+    $stmt = $pdo->prepare($sql);
+    // $stmt->bindParam(":content", $content, PDO::PARAM_STR);
+    $stmt->execute([$content, date("Y/m/d H:i:s")]);
+    // $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+    exit;
+}
+
+// $sql = "SELECT * FROM bbs ORDER BY updated_at;";
+// $stmt = $pdo->prepare($sql);
+// $stmt -> execute();
